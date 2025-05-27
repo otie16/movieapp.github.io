9fa2f71f2988a03849eb8f98c845a4935b35aa51
otie16 <biggieestone@gmail.com>
2025-05-27 23:47:29 +0100

    made some modifications to the the app.js file

--- src/App.js
+++ src/App.js
@@ -6,71 +6,85 @@ import Search from "./components/Search";
 import Result from "./components/Result";
 import Detail from "./components/Detail";
 
-// http://www.omdbapi.com/?i=tt3896198&apikey=6f1b164d
-
-
-
+// OMDb API key and base URL
+const API_KEY = "6f1b164d";
+const BASE_URL = "http://www.omdbapi.com/";
 
 function App() {
-    const [state, setState] = useState({
-        search:'',
-        results:[],
-        selected:{}
-    })
-    const handleInput = (e) => {
-        let search = e.target.value;
-        setState((prevState) => {
-            return{...prevState, search:search}
-        })
-    }
-    const openDetail = (id) => {
-        axios.get('http://www.omdbapi.com/?i='+id+'&apikey=6f1b164d')
-        .then(({data}) => {
-            setState((prevState) => {return {...prevState, selected: data}})
-        })
-        .catch(err => console.log(err))
-    }
+  const [state, setState] = useState({
+    search: "",
+    results: [],
+    selected: {},
+  });
 
-    const SearchResult = (e) => {
-        if(e.key === "Enter") {
-            axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=6f1b164d'+"&s="+state.search)
-            .then(res => {
-                setState(prevState => {
-                    return{...prevState, results: res.data.Search};
-                })
-            })
-            .catch(err => console.log(err))
-        }
+  const handleInput = (e) => {
+    const search = e.target.value;
+    setState((prevState) => ({
+      ...prevState,
+      search: search,
+    }));
+  };
+
+  const openDetail = (id) => {
+    axios
+      .get(`${BASE_URL}?i=${id}&apikey=${API_KEY}`)
+      .then(({ data }) => {
+        setState((prevState) => ({
+          ...prevState,
+          selected: data,
+        }));
+      })
+      .catch((err) => console.error(err));
+  };
+
+  const SearchResult = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(`${BASE_URL}?apikey=${API_KEY}&s=${state.search}`)
+        .then((res) => {
+          setState((prevState) => ({
+            ...prevState,
+            results: res.data.Search || [],
+          }));
+        })
+        .catch((err) => console.error(err));
     }
-    const close = () => {
-        setState((prevState) => {return {...prevState, selected: {}}})
-    }
+  };
+
+  const close = () => {
+    setState((prevState) => ({
+      ...prevState,
+      selected: {},
+    }));
+  };
+
   return (
     <div className="main-wrapper w-100 d-flex flex-column align-items-center min-vh-100">
-        {typeof state.selected.Title !== "undefined" ? <Detail selected={state.selected} close={close}/> : 
-      <header className="w-100 text-center text-white mt-5">
-        <h1><b>Movie Craze</b></h1>
-        <p>I added Github Actions to this</p>
-        <Search handleInput={handleInput} SearchResult={SearchResult} />
-        <div className="container">
+      {typeof state.selected.Title !== "undefined" ? (
+        <Detail selected={state.selected} close={close} />
+      ) : (
+        <header className="w-100 text-center text-white mt-5">
+          <h1>
+            <b>Movie Craze</b>
+          </h1>
+          <p>I added GitHub Actions to this</p>
+          <Search handleInput={handleInput} SearchResult={SearchResult} />
+          <div className="container">
             <div className="row">
-                {
-                    state.results.map((result, i) => (
-                        <div className="col-12 col-sm-6 col-md-3 col-lg-3 my-2">
-                            <Result result = {result} openDetail={openDetail}/>
-                        </div>
-                    ))
-                }
+              {state.results.map((result, i) => (
+                <div
+                  key={i}
+                  className="col-12 col-sm-6 col-md-3 col-lg-3 my-2"
+                >
+                  <Result result={result} openDetail={openDetail} />
+                </div>
+              ))}
             </div>
-        </div>
-      </header>
-      }
+          </div>
+        </header>
+      )}
     </div>
-  )
+  );
 }
+
 export default App;
-
-
-
-
-
